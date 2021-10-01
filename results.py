05b8b30b2024d35d4057ff45bb7e97fd7d2ab40f
Miguel Hernandez <migher74@outlook.com>
2021-09-30 20:47:03 -0500

    Import CONVERTION function and use the "results.txt" file to get the all the numbers

--- results.py
+++ results.py
@@ -1,10 +1,48 @@
-# The results of each draw are divided into lists. The list "a1" is only a greater number that is placed at the end of the concatenation of the lists.
-r1 = [3,4,12,19,23,37]
-r2 = [7,14,21,24,34,36]
-r3 = [13,17,25,28,38,39]
-r4 = [1,8,21,22,34,37]
-r5 = [11,18,25,27,31,39]
-r6 = [1,4,7,10,28,29]
+from minis import convertion
+
+# Ask to the user to enter the 6 numbers of the last week
+n1 = input("Enter the first number...: ")
+n2 = input("Enter the second number..: ")
+n3 = input("Enter the third number...: ")
+n4 = input("Enter the fourth number..: ")
+n5 = input("Enter the fifth number...: ")
+n6 = input("Enter the sixth number...: ")
+
+new_numbers = ("{},{},{},{},{},{}\n".format(n1,n2,n3,n4,n5,n6))
+
+# Open the file to read the previous numbers and  insert the numbers of the last week
+lr = []
+ac_file = open("results.txt","r")
+
+for i in ac_file:
+    lr.append(i)
+
+ac_file.close()
+
+nl0 = new_numbers
+nl1 = lr[0]
+nl2 = lr[1]
+nl3 = lr[2]
+nl4 = lr[3]
+nl5 = lr[4]
+
+nw_file = open("results.txt","w")
+
+nw_file.write(nl0)
+nw_file.write(nl1)
+nw_file.write(nl2)
+nw_file.write(nl3)
+nw_file.write(nl4)
+nw_file.write(nl5)
+
+nw_file.close()
+
+r1 = convertion(nl0)
+r2 = convertion(nl1)
+r3 = convertion(nl2)
+r4 = convertion(nl3)
+r5 = convertion(nl4)
+r6 = convertion(nl5)
 a1 = [100]
 
 # Create and sort a variable concatenating the 6 lists plus the "a1" special list.
