5c5ebada21d950cbdfd336dfcc4e3b9386e1e289
Miguel Hernandez <migher74@outlook.com>
2021-10-03 22:10:45 -0500

    Added ENTER_DATA function - Select the type of game and how many numbers will enter for the las week

--- minis.py
+++ minis.py
@@ -1,3 +1,26 @@
+# This function selects the type of group of numbers according to the type of game
+def enter_data(sel):
+    if sel == 1:
+        label = "FIRST"
+    elif sel == 2:
+        label = "SECOND"
+    else:
+        label = "THIRD"
+    
+    print("Enter the", label, "group of numbers.\n")
+
+    n1 = input("Enter the first number...: ")
+    n2 = input("Enter the second number..: ")
+    n3 = input("Enter the third number...: ")
+    n4 = input("Enter the fourth number..: ")
+    n5 = input("Enter the fifth number...: ")
+    n6 = input("Enter the sixth number...: ")
+
+    new_numbers = ("{},{},{},{},{},{},{}\n".format(sel,n1,n2,n3,n4,n5,n6))
+
+    return new_numbers
+
+
 # This function converts a string into a list
 def convertion(tolist):
     ls = []
